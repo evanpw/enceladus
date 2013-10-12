3bfb4037ece4982cd1dbf883a22a451f21762cb8
Evan Wright <evanpw@gmail.com>
2013-10-12 17:22:35 -0400

    Clean up AST and get rid of some obsolete methods

--- ast.cpp
+++ ast.cpp
@@ -17,12 +17,6 @@ AstNode::~AstNode()
 	delete location_;
 }
 
-void AstNode::show(ostream& out, int depth) const
-{
-	indent(out, depth);
-	out << str() << endl;
-}
-
 ProgramNode* ProgramNode::create()
 {
 	ProgramNode* node = new ProgramNode;
@@ -36,15 +30,6 @@ void ProgramNode::prepend(AstNode* child)
 	children_.push_back(child);
 }
 
-void ProgramNode::show(ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	for (list<AstNode*>::const_iterator i = children_.begin(); i != children_.end(); ++i)
-	{
-		(*i)->show(out, depth + 1);
-	}
-}
-
 LabelNode* LabelNode::create(const char* name)
 {
 	LabelNode* node = new LabelNode;
@@ -55,12 +40,6 @@ LabelNode* LabelNode::create(const char* name)
 	return node;
 }
 
-void LabelNode::show(std::ostream& out, int depth) const
-{
-	indent(out, depth);
-	out << "Label: " << name_ << endl;
-}
-
 NotNode* NotNode::create(ExpressionNode* expression)
 {
 	NotNode* node = new NotNode;
@@ -68,12 +47,6 @@ NotNode* NotNode::create(ExpressionNode* expression)
 
 	node->expression_ = expression;
 	return node;
-}
-
-void NotNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	expression_->show(out, depth + 1);
 }
 
 BinaryOperatorNode* BinaryOperatorNode::create(ExpressionNode* lhs, Operator op, ExpressionNode* rhs)
@@ -87,27 +60,6 @@ BinaryOperatorNode* BinaryOperatorNode::create(ExpressionNode* lhs, Operator op,
 	return node;
 }
 
-void BinaryOperatorNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	lhs_->show(out, depth + 1);
-	rhs_->show(out, depth + 1);
-}
-
-const char* BinaryOperatorNode::str() const
-{
-	switch (op_)
-	{
-	case kPlus: return "BinaryOp(+)";
-	case kMinus: return "BinaryOp(-)";
-	case kTimes: return "BinaryOp(*)";
-	case kDivide: return "BinaryOp(/)";
-	case kMod: return "BinaryOp(mod)";
-	}
-
-	assert(false);
-}
-
 LogicalNode* LogicalNode::create(ExpressionNode* lhs, Operator op, ExpressionNode* rhs)
 {
 	LogicalNode* node = new LogicalNode;
@@ -117,24 +69,6 @@ LogicalNode* LogicalNode::create(ExpressionNode* lhs, Operator op, ExpressionNod
 	node->op_ = op;
 	node->rhs_ = rhs;
 	return node;
-}
-
-void LogicalNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	lhs_->show(out, depth + 1);
-	rhs_->show(out, depth + 1);
-}
-
-const char* LogicalNode::str() const
-{
-	switch (op_)
-	{
-	case kAnd: return "Logical(and)";
-	case kOr: return "Logical(or)";
-	}
-
-	assert(false);
 }
 
 ComparisonNode* ComparisonNode::create(ExpressionNode* lhs, ComparisonNode::Operator op, ExpressionNode* rhs)
@@ -148,28 +82,6 @@ ComparisonNode* ComparisonNode::create(ExpressionNode* lhs, ComparisonNode::Oper
 	return node;
 }
 
-const char* ComparisonNode::str() const
-{
-	switch (op_)
-	{
-	case kGreater: return "Comparison(>)";
-	case kLess: return "Comparison(<)";
-	case kEqual: return "Comparison(=)";
-	case kGreaterOrEqual: return "Comparison(>=)";
-	case kLessOrEqual: return "Comparison(<=)";
-	case kNotEqual: return "Comparison(!=)";
-	}
-
-	assert(false);
-}
-
-void ComparisonNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	lhs_->show(out, depth + 1);
-	rhs_->show(out, depth + 1);
-}
-
 VariableNode* VariableNode::create(const char* name)
 {
 	VariableNode* node = new VariableNode;
@@ -180,12 +92,6 @@ VariableNode* VariableNode::create(const char* name)
 	return node;
 }
 
-void VariableNode::show(std::ostream& out, int depth) const
-{
-	indent(out, depth);
-	out << "Variable: " << name_ << endl;
-}
-
 IntNode* IntNode::create(long value)
 {
 	IntNode* node = new IntNode;
@@ -193,12 +99,6 @@ IntNode* IntNode::create(long value)
 
 	node->value_ = value;
 	return node;
-}
-
-void IntNode::show(std::ostream& out, int depth) const
-{
-	indent(out, depth);
-	out << "Int: " << value_ << endl;
 }
 
 BlockNode* BlockNode::create()
@@ -214,15 +114,6 @@ void BlockNode::prepend(StatementNode* child)
 	children_.push_back(child);
 }
 
-void BlockNode::show(ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	for (list<StatementNode*>::const_iterator i = children_.begin(); i != children_.end(); ++i)
-	{
-		(*i)->show(out, depth + 1);
-	}
-}
-
 IfNode* IfNode::create(ExpressionNode* condition, StatementNode* body)
 {
 	IfNode* node = new IfNode;
@@ -231,13 +122,6 @@ IfNode* IfNode::create(ExpressionNode* condition, StatementNode* body)
 	node->condition_ = condition;
 	node->body_ = body;
 	return node;
-}
-
-void IfNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	condition_->show(out, depth + 1);
-	body_->show(out, depth + 1);
 }
 
 IfElseNode* IfElseNode::create(ExpressionNode* condition, StatementNode* body, StatementNode* else_body)
@@ -251,14 +135,6 @@ IfElseNode* IfElseNode::create(ExpressionNode* condition, StatementNode* body, S
 	return node;
 }
 
-void IfElseNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	condition_->show(out, depth + 1);
-	body_->show(out, depth + 1);
-	else_body_->show(out, depth + 1);
-}
-
 GotoNode* GotoNode::create(const char* target)
 {
 	GotoNode* node = new GotoNode;
@@ -266,12 +142,6 @@ GotoNode* GotoNode::create(const char* target)
 
 	node->target_ = target;
 	return node;
-}
-
-void GotoNode::show(std::ostream& out, int depth) const
-{
-	indent(out, depth);
-	out << "Goto: " << target_ << endl;
 }
 
 PrintNode* PrintNode::create(ExpressionNode* expression)
@@ -283,23 +153,12 @@ PrintNode* PrintNode::create(ExpressionNode* expression)
 	return node;
 }
 
-void PrintNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	expression_->show(out, depth + 1);
-}
-
 ReadNode* ReadNode::create()
 {
 	ReadNode* node = new ReadNode;
 	MemoryManager::addNode(node);
 
 	return node;
-}
-
-void ReadNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
 }
 
 WhileNode* WhileNode::create(ExpressionNode* condition, StatementNode* body)
@@ -312,13 +171,6 @@ WhileNode* WhileNode::create(ExpressionNode* condition, StatementNode* body)
 	return node;
 }
 
-void WhileNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	condition_->show(out, depth + 1);
-	body_->show(out, depth + 1);
-}
-
 AssignNode* AssignNode::create(const char* target, ExpressionNode* value)
 {
 	AssignNode* node = new AssignNode;
@@ -327,16 +179,6 @@ AssignNode* AssignNode::create(const char* target, ExpressionNode* value)
 	node->target_ = target;
 	node->value_ = value;
 	return node;
-}
-
-void AssignNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-
-	indent(out, depth + 1);
-	out << target_ << endl;
-
-	value_->show(out, depth + 1);
 }
 
 FunctionDefNode* FunctionDefNode::create(const char* name, StatementNode* body)
@@ -349,13 +191,6 @@ FunctionDefNode* FunctionDefNode::create(const char* name, StatementNode* body)
 	return node;
 }
 
-void FunctionDefNode::show(std::ostream& out, int depth) const
-{
-	indent(out, depth);
-	out << "Function: " << name_ << endl;
-	body_->show(out, depth + 1);
-}
-
 FunctionCallNode* FunctionCallNode::create(const char* target)
 {
 	FunctionCallNode* node = new FunctionCallNode;
@@ -365,12 +200,6 @@ FunctionCallNode* FunctionCallNode::create(const char* target)
 	return node;
 }
 
-void FunctionCallNode::show(std::ostream& out, int depth) const
-{
-	indent(out, depth);
-	out << "FunctionCall: " << target_ << endl;
-}
-
 ReturnNode* ReturnNode::create(ExpressionNode* expression)
 {
 	ReturnNode* node = new ReturnNode;
@@ -378,10 +207,4 @@ ReturnNode* ReturnNode::create(ExpressionNode* expression)
 
 	node->expression_ = expression;
 	return node;
-}
-
-void ReturnNode::show(std::ostream& out, int depth) const
-{
-	AstNode::show(out, depth);
-	expression_->show(out, depth + 1);
 }
--- ast.hpp
+++ ast.hpp
@@ -17,8 +17,6 @@ class AstNode
 public:
 	AstNode();
 	virtual ~AstNode();
-	virtual const char* str() const  = 0;
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) = 0;
 
 	std::stringstream& code() { return code_; }
@@ -43,8 +41,6 @@ class ProgramNode : public AstNode
 public:
 	static ProgramNode* create();
 	void prepend(AstNode* child);
-	virtual const char* str() const { return "Program"; }
-	virtual void show(std::ostream& out, int depth) const;
 
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
@@ -59,8 +55,6 @@ class LabelNode : public AstNode
 {
 public:
 	static LabelNode* create(const char* name);
-	virtual const char* str() const { return "Label"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	const char* name() { return name_; }
@@ -79,8 +73,6 @@ class NotNode : public ExpressionNode
 {
 public:
 	static NotNode* create(ExpressionNode* expression);
-	virtual const char* str() const { return "Not"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	ExpressionNode* child() { return expression_; }
@@ -95,9 +87,7 @@ class BinaryOperatorNode : public ExpressionNode
 public:
 	enum Operator {kPlus, kMinus, kTimes, kDivide, kMod};
 	static BinaryOperatorNode* create(ExpressionNode* lhs, Operator op, ExpressionNode* rhs);
-	virtual const char* str() const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
-	virtual void show(std::ostream& out, int depth) const;
 
 	ExpressionNode* lhs() { return lhs_; }
 	Operator op() { return op_; }
@@ -115,9 +105,7 @@ class LogicalNode : public ExpressionNode
 public:
 	enum Operator {kAnd, kOr};
 	static LogicalNode* create(ExpressionNode* lhs, Operator op, ExpressionNode* rhs);
-	virtual const char* str() const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
-	virtual void show(std::ostream& out, int depth) const;
 
 	ExpressionNode* lhs() { return lhs_; }
 	Operator op() { return op_; }
@@ -136,9 +124,7 @@ public:
 	enum Operator { kGreater, kEqual, kLess, kGreaterOrEqual, kLessOrEqual, kNotEqual };
 
 	static ComparisonNode* create(ExpressionNode* lhs, Operator op, ExpressionNode* rhs);
-	virtual const char* str() const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
-	virtual void show(std::ostream& out, int depth) const;
 
 	ExpressionNode* lhs() { return lhs_; }
 	Operator op() { return op_; }
@@ -155,8 +141,6 @@ class VariableNode : public ExpressionNode
 {
 public:
 	static VariableNode* create(const char* name);
-	virtual const char* str() const { return "Variable"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	const char* name() { return name_; }
@@ -174,8 +158,6 @@ class IntNode : public ExpressionNode
 {
 public:
 	static IntNode* create(long value);
-	virtual const char* str() const { return "Int"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	long value() { return value_; }
@@ -189,8 +171,6 @@ class FunctionCallNode : public ExpressionNode
 {
 public:
 	static FunctionCallNode* create(const char* target);
-	virtual const char* str() const { return "FunctionCall"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	const char* target() { return target_; }
@@ -204,8 +184,6 @@ class ReadNode : public ExpressionNode
 {
 public:
 	static ReadNode* create();
-	virtual const char* str() const { return "Read"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 private:
@@ -218,8 +196,6 @@ class BlockNode : public StatementNode
 public:
 	static BlockNode* create();
 	void prepend(StatementNode* child);
-	virtual const char* str() const { return "Block"; }
-	virtual void show(std::ostream& out, int depth) const;
 
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
@@ -234,8 +210,6 @@ class IfNode : public StatementNode
 {
 public:
 	static IfNode* create(ExpressionNode* condition, StatementNode* body);
-	virtual const char* str() const { return "If"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	ExpressionNode* condition() { return condition_; }
@@ -251,8 +225,6 @@ class IfElseNode : public StatementNode
 {
 public:
 	static IfElseNode* create(ExpressionNode* condition, StatementNode* body, StatementNode* else_body);
-	virtual const char* str() const { return "IfElse"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	ExpressionNode* condition() { return condition_; }
@@ -270,8 +242,6 @@ class GotoNode : public StatementNode
 {
 public:
 	static GotoNode* create(const char* target);
-	virtual const char* str() const { return "Goto"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	const char* target() const { return target_; }
@@ -285,8 +255,6 @@ class PrintNode : public StatementNode
 {
 public:
 	static PrintNode* create(ExpressionNode* expression);
-	virtual const char* str() const { return "Print"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	ExpressionNode* expression() { return expression_; }
@@ -300,8 +268,6 @@ class WhileNode : public StatementNode
 {
 public:
 	static WhileNode* create(ExpressionNode* condition, StatementNode* body);
-	virtual const char* str() const { return "While"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	ExpressionNode* condition() { return condition_; }
@@ -317,8 +283,6 @@ class AssignNode : public StatementNode
 {
 public:
 	static AssignNode* create(const char* target, ExpressionNode* value);
-	virtual const char* str() const { return "Assign"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	const char* target() { return target_; }
@@ -338,8 +302,6 @@ class FunctionDefNode : public StatementNode
 {
 public:
 	static FunctionDefNode* create(const char* name, StatementNode* body);
-	virtual const char* str() const { return "FunctionDef"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	const char* name() { return name_; }
@@ -359,8 +321,6 @@ class ReturnNode : public StatementNode
 {
 public:
 	static ReturnNode* create(ExpressionNode* expression);
-	virtual const char* str() const { return "Return"; }
-	virtual void show(std::ostream& out, int depth) const;
 	virtual void accept(AstVisitor* visitor) { visitor->visit(this); }
 
 	ExpressionNode* expression() { return expression_; }
