65abe280cc120dc328691d6f2d6116e3e915a6e7
Evan Wright <evanpw@gmail.com>
2015-05-30 20:42:10 -0400

    Re-fix osx compilation

--- osx/build.sh
+++ osx/build.sh
@@ -6,5 +6,5 @@ mkdir -p build
 ./simplec testing/$1.spl > build/$1.asm
 nasm -fmacho64 build/$1.asm -o build/$1.o
 gcc -c lib/library.c -o build/library.o -O -DNDEBUG
-nasm -fmacho64 lib/gc.asm -o build/gc.o
+nasm -fmacho64 lib/gc.asm -d__APPLE__ -o build/gc.o
 gcc -m64 -Wl,-no_pie build/$1.o build/library.o build/gc.o -o build/$1
